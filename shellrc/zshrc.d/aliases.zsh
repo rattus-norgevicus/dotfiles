3e3bafd7478753eaaecc37cfad56e7b3763437f3
rattus-norgevicus <anderson.ibarra@outlook.es>
2025-03-27 18:34:39 -0500

    [feature] ZSH: add aliases

---
+++ shellrc/zshrc.d/aliases.zsh
@@ -0,0 +1,54 @@
+# +----+
+# | ls |
+# +----+
+
+alias ls="ls --color=auto"
+alias ll="ls -lah"
+
+# +-------+
+# | power |
+# +-------+
+
+alias hn="systemctl hybernate"
+alias sn="shutdown now"
+
+# +------------+
+# | move files |
+# +------------+
+
+alias cp="cp -iv"
+alias mv="mv -iv"
+alias rm="rm -iv"
+
+# +------+
+# | grep |
+# +------+
+
+alias grep="grep --color=auto"
+
+# +------+
+# | ping |
+# +------+
+
+alias pg="ping 8.8.8.8"
+
+# +-----+
+# | git |
+# +-----+
+
+alias g="git"
+
+# +------+
+# | nvim |
+# +------+
+
+alias vim="nvim"
+alias vi="nvim"
+
+# +----+
+# | Go |
+# +----+
+
+alias gob="go build"
+alias gor="go run"
+alias gom="go mod tidy"
